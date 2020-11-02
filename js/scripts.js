59046051da0906b64b745b80ea518e3f2be62ff4
Patrick Osten <Patrickosten040@gmail.com>
2020-11-01 17:38:57 -0800

    comment out non functioning code

--- js/scripts.js
+++ js/scripts.js
@@ -23,46 +23,46 @@ function mrRogerizer(n) {
 }
 
 
-// weekend attempts
-const array = [1,2,0,3,7,13,23];
+// // weekend attempts
+// const array = [1,2,0,3,7,13,23];
 
-array.forEach(function(el){
-  let element = array.indexOf(el);
-  if (el == 3) {
-      array.splice(element, 1, "wont you be my neighbor");
-  }
-  else if (el == 2) {
-      array.splice(element, 1, "boop")
-    }
-  else if (el == 1) {
-      array.splice(element, 1, "beep")
-    }
-  });
+// array.forEach(function(el){
+//   let element = array.indexOf(el);
+//   if (el == 3) {
+//       array.splice(element, 1, "wont you be my neighbor");
+//   }
+//   else if (el == 2) {
+//       array.splice(element, 1, "boop")
+//     }
+//   else if (el == 1) {
+//       array.splice(element, 1, "beep")
+//     }
+//   });
 
-  array.forEach(function(el){
-    let element = array.indexOf(el);
-    const regEx = /3+/g 
-    if (el == regEx) { // Why doesn't the if statement work with regular expressions?
-        array.splice(element, 1, "wont you be my neighbor");
-    }
-    else if (el == 2) {
-        array.splice(element, 1, "boop")
-      }
-    else if (el == 1) {
-        array.splice(element, 1, "beep")
-      }
-    });
+//   array.forEach(function(el){
+//     let element = array.indexOf(el);
+//     const regEx = /3+/g 
+//     if (el == regEx) { // Why doesn't the if statement work with regular expressions?
+//         array.splice(element, 1, "wont you be my neighbor");
+//     }
+//     else if (el == 2) {
+//         array.splice(element, 1, "boop")
+//       }
+//     else if (el == 1) {
+//         array.splice(element, 1, "beep")
+//       }
+//     });
 
-// another iteration below
+// // another iteration below
 
-const string = "1,2,3,4,5,13,23,45,43";
+// const string = "1,2,3,4,5,13,23,45,43";
 
-function mrRogerizer(el) {
-  let threeString = el.replace(/3+|\d3+|3\d+/g, "wont you be my neighbor?");
-  let twoString = threeString.replace(/2+|\d2+|2\d+/g, "boop");
-  let oneString = twoString.replace(/1+|\d1+|1\d+/g, "beep");
-  return oneString;
-}
+// function mrRogerizer(el) {
+//   let threeString = el.replace(/3+|\d3+|3\d+/g, "wont you be my neighbor?");
+//   let twoString = threeString.replace(/2+|\d2+|2\d+/g, "boop");
+//   let oneString = twoString.replace(/1+|\d1+|1\d+/g, "beep");
+//   return oneString;
+// }
 
 
 // UI Logic
