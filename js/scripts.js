9d926b9c81c0f68786ebe280014fa0a9b9040f56
Patrick Osten <Patrickosten040@gmail.com>
2020-11-03 17:36:46 -0800

    set upper limit on user input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 //Business Logic
 
 function mrRogerizer(n) {
-  const empty = []
-  for (let i = 0; i < n+1; i++) {
-    empty.push(i);}
-  const string = empty.join(" ");
-  let threeString = string.replace(/3+|\d3+|3\d+/g, "wont you be my neighbor?");
-  let twoString = threeString.replace(/2+|\d2+|2\d+/g, "boop");
-  let oneString = twoString.replace(/1+|\d1+|1\d+/g, "beep");
-  return oneString;
+  if(n<=50){
+    const empty = []
+    for (let i = 0; i < n+1; i++) {
+      empty.push(i);}
+    const string = empty.join(" ");
+    let threeString = string.replace(/3+|\d3+|3\d+/g, "wont you be my neighbor?");
+    let twoString = threeString.replace(/2+|\d2+|2\d+/g, "boop");
+    let oneString = twoString.replace(/1+|\d1+|1\d+/g, "beep");
+    return oneString;
+  }
+  else return "This number is too high to count to neighbor."
 }
 
 // UI Logic
